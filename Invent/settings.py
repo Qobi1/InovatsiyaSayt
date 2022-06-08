1484a16019e9931e955208eee793d6225aca570e
Qobi1 <tolaganov03@list.ru>
2022-06-08 14:19:21 +0500

    l

--- Invent/settings.py
+++ Invent/settings.py
@@ -145,3 +145,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'admin-index'
 LOGOUT_REDIRECT_URL = 'index'
+
+
--- Invent/urls.py
+++ Invent/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
     path('administration/', include('admiin.urls')),
+    path('api/v1/', include('api.urls')),
     path('a/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
